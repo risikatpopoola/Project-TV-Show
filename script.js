f1d521d4ea3631bbc26b72406043d488d4ff9415
Dipa-Sarker <dipasarker696@gmail.com>
2026-08-11 03:28:51 +0100

    add function for episode selector

--- script.js
+++ script.js
@@ -22,16 +22,68 @@
 
 const searchInput = document.querySelector("#search-input"); //get elements from HTML
 const episodeCount = document.querySelector("#episode-count");
+const episodeSelector = document.querySelector("#episode-selector");
+const showAllButton = document.querySelector("#show-all-button"); //for bonus part
 
 let allEpisodes = []; //keep allEpisode outside so that search can use it
 function setup() {
   allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  createEpisodeSelector(allEpisodes);
   makeFooter(); //call makeFooter() outside makePageForEpisodes(episodeList) to display footer
   //only once
 }
 
-function makeFooter() { //add footer function outside 
+function createEpisodeSelector(episodeList) {
+  // for creating episode selector
+  for (const episode of episodeList) {
+    //loop through each episode
+    const optionElement = document.createElement("option"); //create option for each element
+    const episodeCode = `S${String(episode.season).padStart(2, "0")}E${String(
+      episode.number // create episode code
+    ).padStart(2, "0")}`;
+    optionElement.textContent = `${episodeCode} - ${episode.name}`; // create option text
+    optionElement.value = episode.id; //The value of this option should be this episode's ID, suppose
+    //for 1st episode, value is it's id = 4952
+    episodeSelector.append(optionElement); //put the option just created in to the episode selector
+  }
+}
+episodeSelector.addEventListener("change", function (event) {
+  const selectedEpisodeID = event.target.value; // gets the selected episode's ID
+  const selectedEpisode = allEpisodes.find(
+    (
+      episode //find() searches through allEpisodes
+    ) => episode.id === Number(selectedEpisodeID) // It finds the episode where the IDs match.
+    //selectedEpisode now contains the whole episode object
+  );
+  const selectedCard = document.getElementById(
+    //find episode section
+    `episode-${selectedEpisode.id}`
+  );
+  const allCards = document.querySelectorAll("#root section"); //Find all the episode <section> cards inside #root
+  // and store them in allCards
+
+  allCards.forEach(function (card) {
+    //Hide all cards except the selected one
+    // when an episode is selected
+    card.style.display = "none";
+  });
+
+  selectedCard.style.display = "block";
+  selectedCard.scrollIntoView(); // Page jumps to the selected episode
+});
+
+showAllButton.addEventListener("click", function () {
+  //make the "Show All Episodes" button work
+  const allCards = document.querySelectorAll("#root section");
+
+  allCards.forEach(function (card) {
+    card.style.display = "block";
+  });
+});
+
+function makeFooter() {
+  //add footer function outside
   const attribution = document.createElement("p");
   const tvMazeLink = document.createElement("a");
 
@@ -54,13 +106,16 @@ searchInput.addEventListener("input", () => {
   makePageForEpisodes(filteredEpisodes);
 });
 
-function searchEpisodes(searchTerm, allEpisodes) { //only filted episodes 
+function searchEpisodes(searchTerm, allEpisodes) {
+  //only filted episodes
   const filteredEpisodes = allEpisodes.filter((episode) => {
     const name = episode.name.toLowerCase();
     const summary = episode.summary.toLowerCase();
 
-    return name.includes(searchTerm.toLowerCase()) ||
-           summary.includes(searchTerm.toLowerCase());
+    return (
+      name.includes(searchTerm.toLowerCase()) ||
+      summary.includes(searchTerm.toLowerCase())
+    );
   });
 
   return filteredEpisodes;
@@ -72,12 +127,11 @@ function makePageForEpisodes(episodeList) {
 
   rootElem.innerHTML = ""; //for every search it will add only new cards, not olds + new
 
-  episodeCount.textContent = `${episodeList.length} episodes found`; //shows no. of episodes found 
+  episodeCount.textContent = `${episodeList.length} episodes found`; //shows no. of episodes found
   // after each search
 
   const episodeCards = episodeList.map(createEpisodeCard);
   rootElem.append(...episodeCards);
-
 }
 
 function createEpisodeCard(episode) {
@@ -90,6 +144,7 @@ function createEpisodeCard(episode) {
   card.querySelector("img").src = episode.image.medium;
   card.querySelector("summary").innerHTML = episode.summary;
   card.querySelector("#episode-image").alt = episode.name;
+  card.querySelector("section").id = `episode-${episode.id}`;
   return card;
 }
 window.onload = setup;
